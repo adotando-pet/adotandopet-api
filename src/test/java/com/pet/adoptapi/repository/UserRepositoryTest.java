a008cee64df6395bd357608842258d0d6c828f74
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2020-08-06 21:02:07 -0300

    Create test integration with JPA from save a user

---
+++ src/test/java/com/pet/adoptapi/repository/UserRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.pet.adoptapi.repository;
+
+import com.pet.adoptapi.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    UserRepository repository;
+
+    @Test
+    @DisplayName("[SAVE] - Should save a user")
+    public void saveNewBook(){
+        User user = createUserValid();
+
+        User savedUser = repository.save(user);
+
+        assertThat(savedUser.getId()).isNotNull();
+
+    }
+
+    private User createUserValid() {
+        return User.builder()
+                .email("phelliperodrigues.dev@gmail.com")
+                .name("Phellipe Rodrigues")
+                .password("123456")
+                .passwordConfirmation("123456")
+                .build();
+    }
+}
--- src/test/java/com/pet/adoptapi/service/UserServiceTest.java
+++ src/test/java/com/pet/adoptapi/service/UserServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +20,7 @@ import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-
+@ActiveProfiles("test")
 public class UserServiceTest {
 
     UserService service;
